7a89095de7dc3fd78bbdd2876ccd556094b00b52
Nadeem <m.nadeem83005@gmail.com>
2024-05-28 13:02:29 +0530

    Fixing the rename script cos im dumb

--- src-tauri/py/rename.py
+++ src-tauri/py/rename.py
@@ -1,20 +1,20 @@
 from sys import platform
-import platform
 import os
 
 
 if platform == "linux" or platform == "linux2":
-    src_path = f'src-tauri/sidecars/apps'
-    dest_path = f'src-tauri/sidecars/apps-x86_64-unknown-linux-gnu'
-    os.rename()
+    src_path = 'src-tauri/sidecars/apps'
+    dest_path = 'src-tauri/sidecars/apps-x86_64-unknown-linux-gnu'
+    os.rename(src_path, dest_path)
 elif platform == "darwin":
+    import platform
     if platform.processor() == "i386":
-        src_path = f'src-tauri/sidecars/apps'
-        dest_path = f'src-tauri/sidecars/apps-x86_64-apple-darwin'
+        src_path = 'src-tauri/sidecars/apps'
+        dest_path = 'src-tauri/sidecars/apps-x86_64-apple-darwin'
         os.rename(src_path, dest_path)
     elif platform.processor() == "arm":
-        src_path = f'src-tauri/sidecars/apps'
-        dest_path = f'src-tauri/sidecars/apps-aarch64-apple-darwin'
+        src_path = 'src-tauri/sidecars/apps'
+        dest_path = 'src-tauri/sidecars/apps-aarch64-apple-darwin'
         os.rename(src_path, dest_path)
 
 elif platform == "win32":
